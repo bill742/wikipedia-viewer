78ae3f7ba3ada7bf840ecd3b17a306d79459102e
Bill Dean <742designs@gmail.com>
2017-02-03 16:29:29 -0500

    Refining search results

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,40 +9,50 @@ function searchArticle () {
     } else {
       $('p.error').hide();
 
-      // var wikiapi = "https://en.wikipedia.org/w/api.php?action=query&titles=" + inputVal + "&prop=revisions&rvprop=content&format=json";
-
       $.ajax({
         type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + inputVal + "&callback=?",
+        // url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&rvprop=content&section=0&page=" + inputVal + "&callback=?",
+        url: "https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&gsrnamespace=0&gsrsearch=" + inputVal + "&gsrlimit=5&callback=?",
+        // url: "http://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&gsrnamespace=0&gsrsearch=" + inputVal + "&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max",
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
-            var title = data.parse.title;
+            console.log(data);
 
-            var markup = data.parse.text["*"];
-            var blurb = $('<div></div>').html(markup);
+            var title = data.query.title;
+            var pages = data.query.pages;
+
+            // var markup = data.parse.text["*"];
+            // var blurb = $('<div></div>').html(markup);
 
             // Clear out any existing links
             $('.read-more').remove();
 
             // remove links as they will not work
-            blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+            // blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
 
             // remove any references
-            blurb.find('sup').remove();
+            // blurb.find('sup').remove();
 
             // remove cite error
-            blurb.find('.mw-ext-cite-error').remove();
-            $('#output').html($(blurb).find('p'));
+            // blurb.find('.mw-ext-cite-error').remove();
+            // $('#output').html($(blurb).find('p'));
+
+            for (var i in pages) {
+                console.log(pages[i].title);
+                // console.log(pages);
+                // output = ""
+            }
 
             $('.output').addClass('output-visible');
             $('#output p:first-child').after('<p class="read-more"><a href="https://en.wikipedia.org/wiki/' + inputVal + '" target="_blank">Read More</a></p>');
             $('#output p:first-child').css('display', 'block').before('<h2>' + title + '</h2>');
-
         },
         error: function (errorMessage) {
-            console.log('No article found');
+            $('.output').addClass('output-visible');
+            output = "<p>No results found.</p>";
+            document.getElementById('output').innerHTML = output;
         }
     });
   }
