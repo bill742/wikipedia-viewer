dd54dfd5fbb152bf79149ecf60ac6fecdc7210bb
Bill Dean <742designs@gmail.com>
2017-03-04 15:54:07 -0500

    Adding concat to coffee function

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,7 @@ gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({bare: true}))
     .pipe(uglify())
+    .pipe(concat('scripts.js'))
     .pipe(gulp.dest('js'))
     .pipe(browserSync.reload({
       stream: true
